13ba0e1dc535bad4522151cac23dc22536eff736
danilofonte <danilosantosfonte@gmail.com>
2018-02-20 11:15:38 -0300

    [commit] adding marketplace login poc

---
+++ src/main/java/br/com/poc/PocMktpLoginApplication.java
@@ -0,0 +1,12 @@
+package br.com.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocMktpLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocMktpLoginApplication.class, args);
+	}
+}
